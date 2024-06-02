9047a8e35a77632bdbd5a028347d7d1d73d4a0a9
lvoz2 <lv_oz2@icloud.com>
2024-06-03 09:20:25 +1000

    Fix some more stuff in assets/__init__.py

--- assets/__init__.py
+++ assets/__init__.py
@@ -1,18 +1,19 @@
 from pathlib import Path
-from pygame import image
 from typing import Any
+
+from pygame import image
 
 
 def __iterate_files(directory: Path, image_types: list[str]) -> Any:
     """
-        Iterates over files in a directory adding them 
+        Iterates over files in a directory adding them
         to the game assets. Recursively calls itself when
         a sub-directory is found
     Args:
         directory (Path): Path to sub-directory
         image_types (list of strings): tuple of valid image file types
     """
-    assets: Any = {} # I'm not giving this an explicit type hint because it will end up being an n-dimensional dict, where n could be any number
+    assets: Any = {}  # I'm not giving this an explicit type hint because it will end up being an n-dimensional dict, where n could be any number
     for item in Path.iterdir(directory):
         if item.is_file() and item.suffix in image_types:
             assets[item.stem] = image.load(item)
@@ -45,4 +46,3 @@ GAME_ASSETS = load_assets()
 
 
 # def create_asset(name: str) -> pygame.Surface:
-#     

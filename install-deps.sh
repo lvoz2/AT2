a95a729090a752b5487b47eb4c90c6a01bb5851a
lvoz2 <lv_oz2@icloud.com>
2024-06-03 21:54:29 +1000

    Fix random typing/linting errors/warnings

--- install-deps.sh
+++ install-deps.sh
@@ -1,6 +1,6 @@
-python3 -m venv .venv
+pypy -m venv .venv
 source .venv/bin/activate
-python -m pip install --upgrade pip
-python -m pip install flake8 bandit[baseline]
-python -m pip install -U mypy
-if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
+pypy -m pip install --upgrade pip
+pypy -m pip install flake8 bandit[baseline]
+pypy -m pip install -U mypy
+if [ -f requirements.txt ]; then pypy -m pip install -r requirements.txt; fi
--- screen.py
+++ screen.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Optional
+from typing import Any, Callable
 import pygame
 import entity
 
@@ -7,9 +7,9 @@ class Screen:
     def __init__(self, bground: pygame.Surface) -> None:
         self.bground = bground
         self.renderables = None
-        self.active_keys: dict[Any, Callable[[None], None]] = {}
-        self.entities: list[list[Optional[entity.Entity]]] = [[None]]
-        self.ui: list[list[None]] = [[None]]
+        self.active_keys: dict[Any, Callable[[], None]] = {}
+        self.entities: list[list[entity.Entity]] = [[]]
+        self.ui: list[list[entity.Entity]] = [[]]
 
     def register_key(self, key, action: Callable[[None], None]) -> None:
         pass

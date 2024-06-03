b682ffedee87d66242f68a4dfc4f60e011ce023e
lvoz2 <lv_oz2@icloud.com>
2024-06-03 17:05:58 +1000

    Test

--- test.sh
+++ test.sh
@@ -4,9 +4,10 @@ source .venv/bin/activate
 dmypy run -- *.py
 dmypy run -- */*.py
 # stop the build if there are Python syntax errors or undefined names
-flake8 . --exclude ['.venv', '.svn', 'CVS', '.bzr', '.hg', '.git', '__pycache__', '.tox', '.eggs', '*.egg'] --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 . --extend-exclude=".venv" --count --select=E9,F63,F7,F82 --show-source --statistics
 # exit-zero treats all errors as warnings
-flake8 . --exclude ['.venv', '.svn', 'CVS', '.bzr', '.hg', '.git', '__pycache__', '.tox', '.eggs', '*.egg'] --ignore=C901 --count --exit-zero --max-complexity=10 --max-line-length=319 --statistics
+flake8 . --extend-exclude=".venv" --ignore=C901 --count --exit-zero --max-complexity=10 --max-line-length=319 --statistics
 bandit -r *.py
 bandit -r */*.py
-pylint --rcfile ./.pylintrc .
+pylint --rcfile ./.pylintrc *.py
+pylint --rcfile ./.pylintrc */*.py

543a80ce2b79e6bfa30155384cc9822eaa7c909e
lvoz2 <lv_oz2@icloud.com>
2024-06-03 22:01:27 +1000

    Switch from python to pypy

--- install-deps.sh
+++ install-deps.sh
@@ -1,6 +1,6 @@
 pypy -m venv .venv
 source .venv/bin/activate
 pypy -m pip install --upgrade pip
-pypy -m pip install flake8 bandit[baseline]
+pypy -m pip install flake8 bandit[baseline] pylint
 pypy -m pip install -U mypy
 if [ -f requirements.txt ]; then pypy -m pip install -r requirements.txt; fi

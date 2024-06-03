14d5c85320555d21a3b6f8e39f2a87d06fa564f3
lvoz2 <lv_oz2@icloud.com>
2024-06-03 21:33:02 +1000

    Create renderer, lint it and enforce static type checking. Just need to create UI elements and its ready for being run. Wish me luck

--- display.py
+++ display.py
@@ -6,15 +6,40 @@ from singleton import Singleton
 class Display(metaclass=Singleton):
     def __init__(self, cur_screen: str, scr: screen.Screen) -> None:
         self.screens: dict[str, screen.Screen] = {cur_screen: scr}
-        self.cur_screen = cur_screen
+        self.cur_screen = self.screens[cur_screen]
         pygame.init()
         self.window = pygame.display.set_mode((800, 600))
+        self.clock: pygame.time.Clock = pygame.time.Clock()
+        self.delta: list[int] = [0]
+        self.game_over: bool = False
 
     def set_screen(self, new_screen: str) -> None:
         if new_screen in self.screens:
-            self.cur_screen = new_screen
+            self.cur_screen = self.screens[new_screen]
         else:
             raise KeyError("Screen not found, either because it does not exist or hasn't been loaded into the Display")
 
-    def draw(self) -> None:
+    def handle_events(self) -> None:
+        keys = pygame.key.get_pressed()
+        for key in self.cur_screen.active_keys.keys():
+            if key in keys:
+                self.cur_screen.active_keys[key]()
+
+    def update(self, delta: list[int]) -> None:
         pass
+
+    def draw(self) -> None:
+        self.delta.append(self.clock.tick())
+        if len(self.delta) > 10:
+            self.delta = self.delta[(len(self.delta) - 10):]
+        self.window.fill([0, 0, 0])
+        self.window.blit(self.cur_screen.bground, [0, 0])
+        if self.cur_screen.entities != [None]:
+            for entity in self.cur_screen.entities:
+                entity.draw(self.window)
+        if self.cur_screen.ui != [None]:
+            for e in self.cur_screen.ui:
+                e.draw(self.window)
+        self.handle_events()
+        self.update(self.delta)
+        pygame.display.flip()
--- screen.py
+++ screen.py
@@ -1,7 +1,18 @@
+from typing import Any, Callable, Optional
+import pygame
+import entity
+
+
 class Screen:
-    def __init__(self, bground):
+    def __init__(self, bground: pygame.Surface) -> None:
         self.bground = bground
         self.renderables = None
+        self.active_keys: dict[Any, Callable[[None], None]] = {}
+        self.entities: list[list[Optional[entity.Entity]]] = [[None]]
+        self.ui: list[list[None]] = [[None]]
+
+    def register_key(self, key, action: Callable[[None], None]) -> None:
+        pass
 
     def draw(self) -> None:
         pass

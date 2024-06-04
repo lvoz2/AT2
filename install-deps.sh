c734241d1d5b467a2b6ec4be3c9a11bbe587de35
lvoz2 <lv_oz2@icloud.com>
2024-06-04 17:56:37 +1000

    Add all packages as wheel files in ./wheels, tar it up with gzip and ignore the wheels directory

--- install-deps.sh
+++ install-deps.sh
@@ -2,7 +2,8 @@ pypy3 -m venv .venv
 source .venv/bin/activate
 pypy3 -m ensurepip
 pypy3 -m pip install --upgrade pip
-pypy3 -m pip install flake8 bandit[baseline] pylint
-pypy3 -m pip install -U mypy
+tar xf wheels.tar.gz
+pypy3 -m pip install --no-index --find-links=wheels/ flake8 bandit[baseline] pylint
+pypy3 -m pip install --no-index --find-links=wheels/ -U mypy
 if [ -f requirements-loc.txt ]; then pypy3 -m pip install --no-index --find-links=wheels/ -r requirements-loc.txt; fi
 if [ -f requirements.txt ]; then pypy3 -m pip install -r requirements.txt; fi

9758542afc6b1331f59b42c564643d7b0e5bb40e
lvoz2 <lv_oz2@icloud.com>
2024-06-04 19:43:05 +1000

    Change back to pygame being the only wheel present in the repo

--- install-deps.sh
+++ install-deps.sh
@@ -1,9 +1,11 @@
-pypy3 -m venv .venv
+if [ ! -d ./.venv ]; then
+    pypy -m venv .venv
+fi
 source .venv/bin/activate
 pypy3 -m ensurepip
 pypy3 -m pip install --upgrade pip
-tar xf wheels.tar.gz
-pypy3 -m pip install --no-index --find-links=wheels/ flake8 bandit[baseline] pylint
-pypy3 -m pip install --no-index --find-links=wheels/ -U mypy
+tar xf wheels.tar.*
+pypy3 -m pip install flake8 bandit[baseline] pylint
+pypy3 -m pip install -U mypy
 if [ -f requirements-loc.txt ]; then pypy3 -m pip install --no-index --find-links=wheels/ -r requirements-loc.txt; fi
 if [ -f requirements.txt ]; then pypy3 -m pip install -r requirements.txt; fi

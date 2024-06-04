62155cf8bfb926ee5f6e3d774c855d1ac809af79
lvoz2 <lv_oz2@icloud.com>
2024-06-04 12:01:41 +1000

    Add a pygame wheel

--- install-deps.sh
+++ install-deps.sh
@@ -4,12 +4,5 @@ pypy -m ensurepip
 pypy -m pip install --upgrade pip
 pypy -m pip install flake8 bandit[baseline] pylint
 pypy -m pip install -U mypy
-#export PYTHONPATH=/home/mint/.local/lib/pypy3.10/site-packages/
-#git clone https://github.com/pygame/pygame.git
-#cd pygame
-#pypy -m pip install cython
-#pypy3 setup.py -config -auto
-#pypy3 setup.py build
-#pypy3 setup.py install --user
-#cd ..
+if [ -f requirements-loc.txt ]; then pypy -m pip install --no-index --find-links=wheels/ -r requirements-loc.txt; fi
 if [ -f requirements.txt ]; then pypy -m pip install -r requirements.txt; fi

dfeb20147263a5a8f10e346a9ed9c262537962ff
lvoz2 <lv_oz2@icloud.com>
2024-06-04 12:11:05 +1000

    Test

--- install-deps.sh
+++ install-deps.sh
@@ -6,5 +6,6 @@ pypy3 -m pip install flake8 bandit[baseline] pylint
 pypy3 -m pip install -U mypy
 ls /home/mint/actions-runner/runner-1/_work/_temp/
 ls /home/mint/actions-runner/runner-1/_work/_temp/ | grep .sh
+cat /home/mint/actions-runner/runner-1/_work/_temp/*.sh
 if [ -f requirements-loc.txt ]; then pypy3 -m pip install --no-index --find-links=wheels/ -r requirements-loc.txt; fi
 if [ -f requirements.txt ]; then pypy3 -m pip install -r requirements.txt; fi

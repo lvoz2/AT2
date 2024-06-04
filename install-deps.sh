ea394d7d6f438613f5dc5c44fb080dedb2fc1b3b
lvoz2 <lv_oz2@icloud.com>
2024-06-04 10:10:44 +1000

    Test

--- install-deps.sh
+++ install-deps.sh
@@ -6,6 +6,7 @@ pypy -m pip install flake8 bandit[baseline] pylint
 pypy -m pip install -U mypy
 git clone https://github.com/pygame/pygame.git
 cd pygame
+pypy -m pip install cython --user
 pypy3 setup.py -config -auto
 pypy3 setup.py build
 pypy3 setup.py install --user

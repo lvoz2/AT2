db1c77af69259336ca17458f67e382678d19fad9
lvoz2 <lv_oz2@icloud.com>
2024-06-04 10:37:27 +1000

    Test

--- install-deps.sh
+++ install-deps.sh
@@ -4,6 +4,7 @@ pypy -m ensurepip
 pypy -m pip install --upgrade pip
 pypy -m pip install flake8 bandit[baseline] pylint
 pypy -m pip install -U mypy
+export PYTHONPATH=/home/mint/.local/lib/pypy3.10/site-packages/
 git clone https://github.com/pygame/pygame.git
 cd pygame
 pypy -m pip install cython

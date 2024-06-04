92d418f283a334814fe47e2b197ca4d9f2d407cd
lvoz2 <lv_oz2@icloud.com>
2024-06-04 10:08:43 +1000

    Test

--- install-deps.sh
+++ install-deps.sh
@@ -4,10 +4,10 @@ pypy -m ensurepip
 pypy -m pip install --upgrade pip
 pypy -m pip install flake8 bandit[baseline] pylint
 pypy -m pip install -U mypy
-#git clone https://github.com/pygame/pygame.git
-#cd pygame
-#pypy3 setup.py -config -auto
-#pypy3 setup.py build
-#pypy3 setup.py install --user
-#cd ..
+git clone https://github.com/pygame/pygame.git
+cd pygame
+pypy3 setup.py -config -auto
+pypy3 setup.py build
+pypy3 setup.py install --user
+cd ..
 if [ -f requirements.txt ]; then pypy -m pip install -r requirements.txt; fi

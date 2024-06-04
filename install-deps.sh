12a6325c09770eedec394a20fcebea625000ca50
lvoz2 <lv_oz2@icloud.com>
2024-06-04 12:04:48 +1000

    Add wheels directory

--- install-deps.sh
+++ install-deps.sh
@@ -1,8 +1,8 @@
-pypy -m venv .venv
+pypy3 -m venv .venv
 source .venv/bin/activate
-pypy -m ensurepip
-pypy -m pip install --upgrade pip
-pypy -m pip install flake8 bandit[baseline] pylint
-pypy -m pip install -U mypy
-if [ -f requirements-loc.txt ]; then pypy -m pip install --no-index --find-links=wheels/ -r requirements-loc.txt; fi
-if [ -f requirements.txt ]; then pypy -m pip install -r requirements.txt; fi
+pypy3 -m ensurepip
+pypy3 -m pip install --upgrade pip
+pypy3 -m pip install flake8 bandit[baseline] pylint
+pypy3 -m pip install -U mypy
+if [ -f requirements-loc.txt ]; then pypy3 -m pip install --no-index --find-links=wheels/ -r requirements-loc.txt; fi
+if [ -f requirements.txt ]; then pypy3 -m pip install -r requirements.txt; fi

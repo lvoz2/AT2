469cac8b401eea36cc01b18c619d7d100b78fc51
lvoz2 <lv_oz2@icloud.com>
2024-06-06 17:28:08 +1000

    Update testing, removing checks for line length. I'll fix this up later (probably)

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ dmypy run -- *.py */*.py
 # stop the build if there are Python syntax errors or undefined names
 flake8 . --extend-exclude=".venv" --count --select=E9,F63,F7,F82 --show-source --statistics
 # exit-zero treats all errors as warnings
-flake8 . --extend-exclude=".venv" --ignore=C901,F401,F841 --count --exit-zero --max-complexity=10 --max-line-length=319 --statistics
+flake8 . --extend-exclude=".venv" --ignore=E501,C901,F401,F841 --count --exit-zero --max-complexity=10 --statistics
 bandit -r *.py
 bandit -r */*.py
 pylint --rcfile ./.pylintrc *.py

661a84d060c9fd32cb391212cade07769948e23d
lvoz2 <lv_oz2@icloud.com>
2024-06-20 15:03:54 +1000

    Change the test.sh file

--- test.sh
+++ test.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 source .venv/bin/activate
-black *.py
-black */*.py
-isort --profile black *.py
-isort --profile black */*.py
-dmypy run -- *.py */*.py
+black **/*.py
+isort --profile black **/*.py
+dmypy run -- **/*.py
 # stop the build if there are Python syntax errors or undefined names
-flake8 . --extend-exclude=".venv" --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 **/*.py --extend-exclude=".venv" --count --select=E9,F63,F7,F82 --show-source --statistics
 # exit-zero treats all errors as warnings
-flake8 . --extend-exclude=".venv" --ignore=E203,E701,E501,C901,F401,F841,W503 --count --max-line-length=88 --max-complexity=10 --statistics
-bandit -r *.py
-bandit -r */*.py
-pylint --rcfile ./.pylintrc *.py
-pylint --rcfile ./.pylintrc */*.py
+flake8 **/*.py --extend-exclude=".venv" --ignore=E203,E701,E501,C901,F401,F841,W503 --count --max-line-length=88 --max-complexity=10 --statistics
+bandit -r **/*.py
+pylint --rcfile ./.pylintrc **/*.py
